c4db048ace66e8a8721bcd06b9b6135874888b3b
Lorenzo Gentile <lorenzogentile404@gmail.com>
2020-10-26 12:08:29 +0100

    Improved user input

--- latex_notation_stats.py
+++ latex_notation_stats.py
@@ -1,15 +1,30 @@
 import re
+import sys
+import glob
 
-# https://stackoverflow.com/questions/29403784/python-extract-pattern-from-string-using-regex
-# https://stackoverflow.com/questions/12091065/negative-pattern-matching-in-python
+# Get and check user inputs
+if len(sys.argv) < 2:
+    raise ValueError('Please provide paths to files or folder.')
 
-latex_paths = ['introduction.tex', 'preliminaries.tex'] # Insert paths of files here
+if sys.argv[1].endswith('.tex'):
+    # Case latex paths (filter .tex files)
+    latex_paths = list(filter(lambda a: a.endswith('.tex') ,sys.argv[1:]))
+else:
+    # Case latex folder
+    latex_folder = sys.argv[1]
+    latex_paths = glob.glob(latex_folder + '/*.tex')
+
+# latex_paths = ['introduction.tex', 'preliminaries.tex'] # Manually provided latex paths
 
 matches = []
 
 for latex_path in latex_paths:
     latex_file = open(latex_path, "r")
     latex_content = latex_file.read()
+
+    # https://stackoverflow.com/questions/29403784/python-extract-pattern-from-string-using-regex
+    # https://stackoverflow.com/questions/12091065/negative-pattern-matching-in-python
+
     pattern = re.compile(r'\$.*?(?!\\).\$')
     matches += [match for match in re.findall(pattern, latex_content)]
 
@@ -41,3 +56,6 @@ print(r1)
 
 r2 = pysqldf("SELECT * FROM stats_table WHERE length(math_expression) < 20 AND (isolated_occurences > 1 OR occurences_as_substring > 1) ORDER BY isolated_occurences, occurences_as_substring, -length(math_expression);")
 pretty_print(r2)
+
+# Remind considered files
+print('\nConsidered files: ' + str(latex_paths))
